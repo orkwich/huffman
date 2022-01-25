598386b72804c68f25445e463cef14ed0790fa42
orkwich <58514302+orkwich@users.noreply.github.com>
2022-01-25 16:50:57 +0100

    Add files via upload

---
+++ src/test/java/HuffmanTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class HuffmanTest {
+    @Test
+    public void testHuffmanComp()
+    {
+        Huffman huf = new Huffman();
+        assertTrue(huf.huffman("txt/niepewnosc.txt", true)>0);
+    }
+    @Test
+    public void testHuffmanDeComp()
+    {
+        System.out.println("git");
+        Huffman huf = new Huffman();
+        assertTrue(huf.huffman("compressed", false)>0);
+    }
+}
