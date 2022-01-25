c335c3e1d4aecc04c39172217f4ea99588f90f98
orkwich <58514302+orkwich@users.noreply.github.com>
2022-01-25 16:49:04 +0100

    Add files via upload

---
+++ src/main/java/exceptions/NoDataToCompressException.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class NoDataToCompressException extends RuntimeException{
+    public NoDataToCompressException(String message){super(message);}
+}
---
+++ src/main/java/exceptions/NotInitializedException.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class NotInitializedException extends RuntimeException{
+    public NotInitializedException(String message) {super(message);}
+}
